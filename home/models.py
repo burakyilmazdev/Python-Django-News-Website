13c473368e4250d945cc08974c95941c51b7ea5f
burakyilmazdev <burakyilmaz1627@gmail.com>
2020-04-05 05:13:17 +0300

    Contact Form eklendi

--- home/models.py
+++ home/models.py
@@ -1,8 +1,9 @@
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
-
 # Create your models here.
+from django.forms import Textarea, TextInput, ModelForm
+
 
 class Setting(models.Model):
     STATUS = (
@@ -31,3 +32,35 @@ class Setting(models.Model):
 
     def _str_(self):
         return self.title
+
+
+class ContactFormMessage(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('Read', 'Read'),
+        ('Closed', 'Closed'),
+    )
+    name = models.CharField(blank=True, max_length=20)
+    email = models.CharField(blank=True, max_length=150)
+    subject = models.CharField(blank=True, max_length=50)
+    message = models.CharField(blank=True, max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    ip = models.CharField(blank=True, max_length=20)
+    note = models.CharField(blank=True, max_length=100)
+    create_at = models.DateTimeField(blank=True, auto_now_add=True)
+    update_at = models.DateTimeField(blank=True, auto_now=True)
+
+    def _str_(self):
+        return self.name
+
+
+class ContactFormu(ModelForm):
+    class Meta:
+        model = ContactFormMessage
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': TextInput(attrs={'class': 'input', 'placeholder': 'Name & Surname'}),
+            'subject': TextInput(attrs={'class': 'input', 'placeholder': 'Subject'}),
+            'email': TextInput(attrs={'class': 'input', 'placeholder': 'Email Address'}),
+            'message': Textarea(attrs={'class': 'input', 'placeholder': 'Your Message', 'rows': '5'}),
+        }
--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
@@ -16,8 +17,22 @@ def aboutus(request):
     context = {'setting': setting, 'page': 'aboutus'}
     return render(request, 'aboutus.html', context)
 
-def contact(request):
-    setting = Setting.objects.get(pk=1)
-    context = {'setting': setting, 'page': 'aboutus'}
-    return render(request, 'contact.html', context)
 
+def contact(request):
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Mesajınız gönderildi")
+            return HttpResponseRedirect('/contact')
+
+    setting = Setting.objects.get(pk=1)
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}
+    return render(request, 'contact.html', context)
