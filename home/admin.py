bd1cda516cf10da8c48453647ae82da5f0baa766
burakyilmazdev <burakyilmaz1627@gmail.com>
2020-04-25 06:45:14 +0300

    User profile yapıldı.

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from home.models import Setting, ContactFormMessage
+from home.models import Setting, ContactFormMessage, UserProfile
 
 
 class ContactFormMessageAdmin(admin.ModelAdmin):
@@ -9,5 +9,10 @@ class ContactFormMessageAdmin(admin.ModelAdmin):
     list_filter = ['status']
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'phone', 'address', 'city', 'country', 'image_tag']
+
+
 admin.site.register(ContactFormMessage, ContactFormMessageAdmin)
 admin.site.register(Setting)
+admin.site.register(UserProfile, UserProfileAdmin)
